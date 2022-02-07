271b9308c27cffb93be7bd3c05ed9629fd4de1a1
lanaya2115 <htl.lind@gmail.com>
2022-02-07 14:29:55 +0700

    [Feature] - complete final assignment

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = parseInt(document.getElementById("principal").value);
+
+    // check value of principle
+    if (principal <= 0){
+        alert("Enter a positive number!")
+        document.getElementById("principal").focus()
+        return
+    }
+
+    //rate
+    var rate = document.getElementById("rate").value;
+
+    //years
+    var years = document.getElementById("years").value;
+
+    //interest
+    var interest = principal * years * rate /100;
+
+    //actual year
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //Html for visualize
+    var finalHtml = `</br><b>If</b> you deposit <label>${principal}</label>,</br>
+    <b>at</b> an interest rate of <label>${rate}%</label>.</br>
+    <b>You</b> will receive an amount of <label>${interest}</label>,</br>
+    <b>in</b> the year <label>${year}</label>`
+    document.getElementById("result").innerHTML = finalHtml
+}
+
+//update rate value
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
